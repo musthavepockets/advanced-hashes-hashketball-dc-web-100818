5fd59ea43f7d2ba14f579ef0880438ba06eda755
cd <coraldarling@outlook.com>
2018-10-03 17:42:53 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -73,7 +73,7 @@ def player_stats(player_name)
         data.each do |name, player_data|
           player_data.each do |key, value|
           if name == player_name
-            statistics.mergeplayer_data
+            statistics.merge(player_data)
             binding.pry
           end
         end
